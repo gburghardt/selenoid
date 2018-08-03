99bbf3ae2912efd17ef26aa751713e950e6dca7f
Greg Burghardt <burghardt.greg@gmail.com>
2018-08-03 10:57:42 -0400

    Renamed test class

--- Tests/PageModelTests.cs
+++ Tests/PageModelTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Selenoid.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class PageModelTests
     {
         [TestMethod]
         public void TestMethod1()
